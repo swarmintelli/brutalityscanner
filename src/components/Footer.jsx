90d43b4ba9ea0c576d1e67be3e3435d6644746cc
shakeebshams <shakeebshams@gmail.com>
2020-06-05 01:04:59 -0400

    Build Header and footer with formatting

---
+++ src/components/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Footer() {
+    return <footer>Built in Atlanta</footer>;
+}
+
+export default Footer;
+
--- src/index.js
+++ src/index.js
@@ -12,5 +12,6 @@
 //HINT: You will need to study the classes in teh styles.css file to appy styling.
 import React from 'react';
 import ReactDOM from 'react-dom'
+import App from './components/App'
 
-ReactDOM.render(<h1>bruh</h1>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
