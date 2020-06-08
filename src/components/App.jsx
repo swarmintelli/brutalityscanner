7b4425d415f3c739f908042c988ab2a37cd0f86f
shakeebshams <shakeebshams@gmail.com>
2020-06-07 20:37:40 -0400

    core algo for keeper add completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,35 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import notes from '../notes';
+//import notes from '../notes';
+import CreateArea from './CreateArea';
 
 function App() {
-    return(
+  var [notes, setNotes] = React.useState([]);
+
+  function addNote(note) {
+    setNotes((prev) => {
+      return [...prev, note];
+    })
+  }
+
+  function deleteNote(id) {
+    console.log('getting here');
+    setNotes((prev) => {
+      return prev.filter((noteItem, index) => {
+        return index !== id;
+      })
+    })
+  }
+
+  return(
     <div>
       <Header />
-      {notes.map(x => <Note title={x.title} content={x.content}/>)}
+      <CreateArea onAdd={addNote}/>
+      {notes.map((x, index) => <Note id={index} title={x.title} content={x.content} onDelete={deleteNote}/>)}
       <Footer />
-    </div>);
+    </div>
+  );
 }
 
 export default App;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
 function Note(prop) {
+    function handleClick() {
+        prop.onDelete(prop.id);
+    }
+
     return(
         <div className='note'>
             <h1>{prop.title}</h1>
             <p>{prop.content}</p>
+            <button onClick={handleClick}>DELETE</button>
         </div>
     );
 }
