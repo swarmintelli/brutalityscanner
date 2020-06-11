62b1583d13d00cb3f398d892faa0a9431b1cb461
shakeebshams <shakeebshams@gmail.com>
2020-06-11 02:29:52 -0400

    Add react routing to navbar links

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-//import notes from '../notes';
 import CreateArea from './CreateArea';
+import Upload from './Upload';
+import Stats from './Stats'
+import About from './About';
+import Video from './Video';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   var [notes, setNotes] = React.useState([]);
@@ -15,7 +19,6 @@ function App() {
   }
 
   function deleteNote(id) {
-    console.log('getting here');
     setNotes((prev) => {
       return prev.filter((noteItem, index) => {
         return index !== id;
@@ -26,6 +29,15 @@ function App() {
   return(
     <div>
       <Header />
+      <Router>
+        <Switch>
+          <Route path='/' exact component={App} />
+          <Route path='/upload' exact component={Upload} />
+          <Route path='/stats' exact component={Stats} />
+          <Route path='/about' exact component={About} />
+          <Route path='/video/:id' component={Video} />
+        </Switch>
+      </Router>
       <CreateArea onAdd={addNote}/>
       {notes.map((x, index) => <Note id={index} title={x.title} content={x.content} onDelete={deleteNote}/>)}
       <Footer />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
-import EventNoteIcon from '@material-ui/icons/EventNote';
+import GavelIcon from '@material-ui/icons/Gavel';
+import {Link} from 'react-router-dom';
 
 function Header() {
-    return <header><EventNoteIcon />Card Keeper</header>
+    return(
+        <div className="totalheader">
+            <header className="titleheader"><GavelIcon />Brutality Scanner</header>
+            <header className="navheader">
+                <Link to='/'><div class='routinglinks'>Home</div></Link>
+                <Link to='/upload'><div class='routinglinks'>Upload</div></Link>
+                <Link to='/stats'><div class='routinglinks'>Stats</div></Link>
+                <Link to='/about'><div class='routinglinks'>About</div></Link>
+            </header>
+        </div>
+    );
 }
 
 export default Header;
